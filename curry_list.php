a3f88a248e31dc770acc06a7779cccc1525c4912
undew <08taka22s@gmail.com>
2019-07-31 10:08:49 +0900

    レイアウト上げ直し

---
+++ curry_list.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="jn" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/curry_about.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <title></title>
+  </head>
+  <body>
+    <div id="container">
+      <div id="header">
+        <div id="toppage">
+        <p><a href="curry_result.html">カレー一覧へ戻る</a></p>
+        <p><a href="index.html">トップページ</a></p>
+        </div>
+      </div>
+      <div id="result_image">
+        <div id="curry_image">
+          <p><img src="img/curry.png"></p>
+        </div>
+        <div id="color_image">
+          <p class="color">　</p>
+        </div>
+      </div>
+      <div id="description">
+        <h1>コメント欄</h1>
+        <div id="curry_comment">
+        <table class="user_comment">
+          <tr>
+            <td>1</td><td class="user_name">andy</td><td>2019/07/30</td>
+          </tr>
+          <tr>
+            <td colspan="3">安藤貴士だよ</td>
+          </tr>
+        </table>
+        <table class="user_comment">
+          <tr>
+            <td>1</td><td class="user_name">andy</td><td>2019/07/30</td>
+          </tr>
+          <tr>
+            <td colspan="3">安藤貴士だよ</td>
+          </tr>
+        </table>
+        <table class="user_comment">
+          <tr>
+            <td>1</td><td class="user_name">andy</td><td>2019/07/30</td>
+          </tr>
+          <tr>
+            <td colspan="3">安藤貴士だよ</td>
+          </tr>
+        </table>
+        <table class="user_comment">
+          <tr>
+            <td>1</td><td class="user_name">andy</td><td>2019/07/30</td>
+          </tr>
+          <tr>
+            <td colspan="3">安藤貴士だよ</td>
+          </tr>
+        </table>
+        <table class="user_comment">
+          <tr>
+            <td>1</td><td class="user_name">andy</td><td>2019/07/30</td>
+          </tr>
+          <tr>
+            <td colspan="3">安藤貴士だよ</td>
+          </tr>
+        </table>
+        <table class="user_comment">
+          <tr>
+            <td>1</td><td class="user_name">andy</td><td>2019/07/30</td>
+          </tr>
+          <tr>
+            <td colspan="3">安藤貴士だよ</td>
+          </tr>
+        </table>
+        <table class="user_comment">
+          <tr>
+            <td>1</td><td class="user_name">andy</td><td>2019/07/30</td>
+          </tr>
+          <tr>
+            <td colspan="3">安藤貴士だよ</td>
+          </tr>
+        </table>
+
+        </div>
+        <table class="board_table">
+          <tr>
+            <td>ニックネーム</td><td><input type="text" name=""></td>
+          </tr>
+          <tr>
+            <td>コメント</td><td><input type="text" name="" value=""></td>
+          </tr>
+          <tr>
+            <th colspan="2" class="button_submit"><button type="submit">コメント！</button></th>
+          </tr>
+        </table>
+      </div>
+      <div id="board_form">
+        <h1 class="nickname">ニックネーム</h1>
+        <p class="board_click">投稿しよう！</p>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ js/query_result.js
@@ -0,0 +1,33 @@
+$(function(){
+
+  $('.board_click').click(
+    function(){
+      $('#board_form').css('top','-15%');
+      $('#board_form').css('transition','0.3s');
+      $('.board_table').css('opacity','1');
+      $('.board_table').css('z-index','99');
+      $('.board_table').css('transition','0.3s');
+      $('.board_click').css('line-height','225px');
+      $('.board_click').css('transition','0.3s');
+      $('.board_click').css('color','white');
+      $('.board_click').css('z-index','-99');
+    }
+  );
+  $('.button_submit').click(
+    function(){
+      $('#board_form').css('top','10%');
+      $('#board_form').css('transition','0.3s');
+      $('.board_table').css('opacity','0');
+      $('.board_table').css('z-index','-99');
+      $('.board_table').css('transition','0.3s');
+      $('.board_click').css('line-height','225px');
+      $('.board_click').css('transition','0.3s');
+      $('.board_click').css('color','white');
+      $('.board_click').css('z-index','99');
+      $('.board_click').css('color','black');
+      $('.board_click').html('<a href="curry_result.html"><p class="board_click">掲示板へ</p></a>');
+
+    }
+  );
+
+});
