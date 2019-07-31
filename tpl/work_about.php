04280d9c0a2b520541d968569f6ae9dedcc2bf27
undew <08taka22s@gmail.com>
2019-07-31 16:14:38 +0900

    ユーザーの作った作品を参照する画面

---
+++ tpl/work_about.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="jn" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="work_about.css">
+    <script src="./js/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="js/query_result.js"></script>
+    <title></title>
+  </head>
+  <body>
+    <div id="container">
+      <div id="header">
+        <p>カレー一覧へもどる</p>
+      </div>
+      <div id="result_image">
+        <div id="curry_image">
+          <p><img src="curry.png"></p>
+        </div>
+      </div>
+      <div id="description">
+        <h1>コメント欄</h1>
+        <table>
+          <tr>
+            <td>1</td><td>ニックネーム</td><td>2019/07/31</td>
+          </tr>
+          <tr>
+            <td colspan="3">コメント</td>
+          </tr>
+        </table>
+        <table>
+          <tr>
+            <td>1</td><td>ニックネーム</td><td>2019/07/31</td>
+          </tr>
+          <tr>
+            <td colspan="3">コメント</td>
+          </tr>
+        </table>
+        <table>
+          <tr>
+            <td>1</td><td>ニックネーム</td><td>2019/07/31</td>
+          </tr>
+          <tr>
+            <td colspan="3">コメント</td>
+          </tr>
+        </table>
+        <table>
+          <tr>
+            <td>1</td><td>ニックネーム</td><td>2019/07/31</td>
+          </tr>
+          <tr>
+            <td colspan="3">コメント</td>
+          </tr>
+        </table>
+        <table>
+          <tr>
+            <td>1</td><td>ニックネーム</td><td>2019/07/31</td>
+          </tr>
+          <tr>
+            <td colspan="3">コメント</td>
+          </tr>
+        </table>
+      </div>
+       <div class="board_table">
+        <table>
+          <tr>
+            <td>ニックネーム</td><td><input type="text" name="" value=""></td>
+          </tr>
+          <tr>
+            <td>コメント</td><td><input type="text" name="" value=""></td>
+          </tr>
+          <tr>
+            <th colspan="2" class="button_submit"><button type="submit">コメント！</button></th>
+          </tr>
+        </table>
+        </div>
+        <div id="title">
+          <p class="title_name">ニックネーム</p>
+          <p class="title_info">説明文</p>
+        </div>
+      </div>
+  </body>
+</html>
